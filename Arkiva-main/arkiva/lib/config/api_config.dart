e0ade82bfac28a1cf3d6256ddcc65f844ad00dd0
unknown <braveemg@gmail.com>
2025-07-30 12:16:47 +0100

    m

--- Arkiva-main/arkiva/lib/config/api_config.dart
+++ Arkiva-main/arkiva/lib/config/api_config.dart
@@ -5,10 +5,12 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 class ApiConfig {
   static String get baseUrl {
     //if (kIsWeb) {
-      return 'http://localhost:3000';
-    // } else {
-    //   return 'http://192.168.1.147:3000';
-    // }
+      return 'http://localhost:3001';
+    //} else {
+      //return 'http://192.168.1.147:3000';
+      // return 'http://192.168.100.112:3000';
+
+    //}
   }
 
   static Future<Map<String, String>> getHeaders() async {
