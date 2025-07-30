686d38fd6ed471bf89c2976febe9994193be0517
aafissou <afissou.assouma@epitech.eu>
2025-07-30 10:07:48 +0100

    m

--- Arkiva-main/arkiva/lib/services/image_processing_mobile.dart
+++ Arkiva-main/arkiva/lib/services/image_processing_mobile.dart
@@ -358,7 +358,7 @@ class ImageProcessingService {
       [0.0, -1.0, 0.0]
     ];
     
-    return img.convolution(image, kernel: kernel);
+    return img.convolution(image, kernel);
   }
 
   /// Convertit une image en PDF
